a5ca6dc9ba215f65fc3d36c8d5dbe3eea71cee7c
DiegoPantaU <74800768+DiegoPantaU@users.noreply.github.com>
2022-09-30 22:11:26 -0500

    Update Reservas.cs

--- Models/Reservas.cs
+++ Models/Reservas.cs
@@ -2,5 +2,15 @@
 {
     public class Reservas
     {
+
+        [Key]
+        public int id_reserva { get; set; }
+        public string? id_usuario { get; set; }
+        public string? id_doctor { get; set; }
+        [Required]
+        public string? especialidad { get; set; }
+        public date fecha { get; set; }
+        [Required]
+        
     }
 }
